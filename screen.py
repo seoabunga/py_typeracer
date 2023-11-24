342b3f81f8cee2ba8ec0fb748d20ee8770050e7b
Brian <brian.ssh725@gmail.com>
2023-11-23 17:27:39 -0800

    game start text

--- screen.py
+++ screen.py
@@ -1,11 +1,18 @@
 import curses
 from curses import wrapper
 
+def start(stdscr):
+    stdscr.erase()
+    stdscr.addstr("Welcome to the Speed Typing Test")
+    stdscr.addstr("\nPress any key to begin.")
+    stdscr.refresh()
+    stdscr.getkey()
 
 def main(stdscr):
-    stdscr.erase()
-    stdscr.addstr("Hello World!")
-    stdscr.refresh()
-    stdscr.getkey()     # waits for user input
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    
+    start(stdscr)
 
 wrapper(main)
