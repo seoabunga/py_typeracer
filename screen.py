73fb2f05a5c39cb3c7a667ee06b47cc5908f1398
Brian <brian.ssh725@gmail.com>
2023-11-23 17:53:00 -0800

    handling correct and incorrect user input

--- screen.py
+++ screen.py
@@ -8,16 +8,23 @@ def start(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def display_text(stdscr, target, current, wpm = 0):
+    stdscr.addstr(target)
+
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addstr(0, i, char, color)
+
 def wpm_test(stdscr):
-    target_text = "Hello world this is some test text for this app!\n"
+    target_text = "Hello world this is some test text for this app!"
     current_text = []
 
     while True:
         stdscr.erase()
-        stdscr.addstr(target_text)
-
-        for char in current_text:
-            stdscr.addstr(char, curses.color_pair(1))
+        display_text(stdscr, target_text, current_text)
         stdscr.refresh()
         
         input = stdscr.getkey()
