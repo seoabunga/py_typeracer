8b0753c10b954dc9002e67624e93d2f60cfc1d1b
Brian <brian.ssh725@gmail.com>
2023-11-23 17:44:50 -0800

    handle user input

--- screen.py
+++ screen.py
@@ -8,11 +8,33 @@ def start(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for this app!\n"
+    current_text = []
+
+    while True:
+        stdscr.erase()
+        stdscr.addstr(target_text)
+
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+        stdscr.refresh()
+        
+        input = stdscr.getkey()
+
+        # handling backspace in different os
+        if input in ("KEY_BACKSPACE", "\b", "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(input)
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
     start(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main)
