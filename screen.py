182e8a6a94bdd6f695c7a81e717d204c431dbe79
Brian <brian.ssh725@gmail.com>
2023-11-23 17:01:57 -0800

    hello world

--- screen.py
+++ screen.py
@@ -1,3 +1,11 @@
 import curses
 from curses import wrapper
 
+
+def main(stdscr):
+    stdscr.erase()
+    stdscr.addstr("Hello World!")
+    stdscr.refresh()
+    stdscr.getkey()     # waits for user input
+
+wrapper(main)
